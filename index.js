ce3d31d88f32ee2359aaa992595eb635e7951e11
hyerim <kimhyerim511@gmail.com>
2024-01-23 15:58:24 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import fs from "node:fs/promises";
+
+import bodyParser from "body-parser";
+import express from "express";
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+app.get("/projects", async (req, res) => {
+  const projects = await fs.readFile("./data/project.json", "utf8");
+  res.json(JSON.parse(projects));
+});
+
+app.use((req, res) => {
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  res.status(404).json({ message: "Not found" });
+});
+
+app.listen(3000);
