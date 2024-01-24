3f0c6bd9abc228c8104aeeb9d64155b5387b828c
hyerim <kimhyerim511@gmail.com>
2024-01-23 18:20:45 -0800

    test json

--- index.js
+++ index.js
@@ -31,24 +31,15 @@
 // app.listen(3000);
 
 const express = require("express");
-const bodyParser = require("body-parser");
-const fs = require("node:fs/promises");
+// const bodyParser = require("body-parser");
+// const fs = require("node:fs/promises");
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(express.static("public"));
-
-app.use((req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-});
-
 app.get("/", (req, res) => {
-  const projects = fs.readFile("./data/project.json", "utf8");
-  const data = res.json(JSON.parse(projects));
-  res.send(data);
+  // const projects = fs.readFile("./data/project.json", "utf8");
+  // res.json(JSON.parse(projects));
+  res.send("test");
 });
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
