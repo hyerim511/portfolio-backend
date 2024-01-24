396f76e8c35f838cb922d89641dd5d000d2d26c4
hyerim <kimhyerim511@gmail.com>
2024-01-23 17:00:17 -0800

    test for api

--- api/index.js
+++ api/index.js
@@ -1,31 +1,48 @@
-import fs from "node:fs/promises";
+// import fs from "node:fs/promises";
 
-import bodyParser from "body-parser";
-import express from "express";
+// import bodyParser from "body-parser";
+// import express from "express";
 
-const app = express();
+// const app = express();
 
-app.use(bodyParser.json());
-app.use(express.static("public"));
+// app.use(bodyParser.json());
+// app.use(express.static("public"));
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// });
+
+// app.get("/api", async (req, res) => {
+//   const projects = await fs.readFile("./data/project.json", "utf8");
+//   res.json(JSON.parse(projects));
+// });
+
+// app.use((req, res) => {
+//   if (req.method === "OPTIONS") {
+//     return res.sendStatus(200);
+//   }
+
+//   res.status(404).json({ message: "Not found" });
+// });
+
+// app.listen(3000);
+
+const app = require("express")();
+const { v4 } = require("uuid");
+
+app.get("/api", (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
 
-app.get("/api", async (req, res) => {
-  const projects = await fs.readFile("./data/project.json", "utf8");
-  res.json(JSON.parse(projects));
+app.get("/api/item/:slug", (req, res) => {
+  const { slug } = req.params;
+  res.end(`Item: ${slug}`);
 });
 
-app.use((req, res) => {
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-
-  res.status(404).json({ message: "Not found" });
-});
-
-app.listen(3000);
+module.exports = app;
