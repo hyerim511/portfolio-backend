90a6ea535ff6afd3a127aad1d3d057aa79be5dce
hyerim <kimhyerim511@gmail.com>
2024-01-23 17:12:56 -0800

    test for api

--- index.js
+++ index.js
@@ -3,29 +3,39 @@ import fs from "node:fs/promises";
 import bodyParser from "body-parser";
 import express from "express";
 
+// const app = express();
+
+// app.use(bodyParser.json());
+// app.use(express.static("public"));
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// });
+
+// app.get("/", async (req, res) => {
+//   const projects = await fs.readFile("./data/project.json", "utf8");
+//   res.json(JSON.parse(projects));
+// });
+
+// app.use((req, res) => {
+//   if (req.method === "OPTIONS") {
+//     return res.sendStatus(200);
+//   }
+
+//   res.status(404).json({ message: "Not found" });
+// });
+
+// app.listen(3000);
+
+const express = require("express");
 const app = express();
-
-app.use(bodyParser.json());
-app.use(express.static("public"));
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
 });
-
-app.get("/", async (req, res) => {
-  const projects = await fs.readFile("./data/project.json", "utf8");
-  res.json(JSON.parse(projects));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
-
-app.use((req, res) => {
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-
-  res.status(404).json({ message: "Not found" });
-});
-
-app.listen(3000);
